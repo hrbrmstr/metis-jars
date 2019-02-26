78838be2da37042738175d000ca24d92200429b2
hrbrmstr <bob@rud.is>
2019-02-26 12:46:57 -0500

    log

--- R/zzz.R
+++ R/zzz.R
@@ -19,6 +19,7 @@
 .onLoad <- function(libname, pkgname) {
 
   rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
+  rJava::.jpackage(pkgname, lib.loc = libname)
   rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
 
 }
